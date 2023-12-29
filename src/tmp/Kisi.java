8213cf976644a50b7bcd031ee73c310ecf6794d8
mfadak <fatih@returnsoft.com.tr>
2023-12-29 22:19:09 +0300

    kisi sinifina boy ozelligi eklendi

--- src/tmp/Kisi.java
+++ src/tmp/Kisi.java
@@ -3,16 +3,21 @@ package tmp;
 public class Kisi{
   private String isim;
   private double yas;
+  private double boy;
 
-  public Kisi(String isim, double yas){
+  public Kisi(String isim, double yas,double boy){
     this.isim = isim;
     this.yas = yas;
+    this.boy = boy;
+  }
+  public void sporYap(double saat) {
+	  boy += (saat/100);
   }
   public void yaslan(){
     yas++;
   }
   @Override
   public String toString(){
-    return isim + " " + yas;
+    return isim + " " + yas + " " + boy;
   }
 }
--- src/tmp/Program.java
+++ src/tmp/Program.java
@@ -2,8 +2,9 @@ package tmp;
 
 public class Program{
   public static void main(String[] args){
-    Kisi kisi = new Kisi("Mehmet",25);
+    Kisi kisi = new Kisi("Mehmet",25, 178);
     kisi.yaslan();
+    kisi.sporYap(5);
     System.out.println(kisi);
   }
 }
